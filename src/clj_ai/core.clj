87da64efcf280a352007f364347ba79c1c5b14ba
Doug Knight <dougbknight@gmail.com>
2017-02-04 23:28:13 -0600

    start validating inputs

--- src/clj_ai/core.clj
+++ src/clj_ai/core.clj
@@ -1,6 +1,36 @@
-(ns clj-ai.core)
+(ns clj-ai.core
+  (:require [clojure.spec :as s]
+            [clojure.pprint :refer [pprint]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn- network?
+  [network]
+  (when (and (vector? network)
+             (every? vector? network))
+    (let [row-count (-> network first count)
+          col-count (-> network first first count)]
+      (every? (fn [matrix]
+                (and
+                  (= row-count (count matrix))
+                  (every? (fn [row]
+                            (and (= col-count (count row))
+                                 (every? number? row)))
+                          matrix)))
+              network))))
+
+(s/def ::network network?)
+(s/def ::input (s/coll-of number?))
+
+(defn train
+  "Accepts a matrix of link weights and some
+  input data and returns the updates matrix
+  of weights"
+  [network input]
+  {:network (s/valid? network)
+   :input (s/valid? input)})
+
+(defn query
+  "Accepts a matrix of link weights an some
+  input and returns the neural network output"
+  [network input]
+  (when-not (s/valid? ::network network)
+    (throw (ex-info "The network is not valid" {:network network}))))
--- test/clj_ai/core_test.clj
+++ test/clj_ai/core_test.clj
@@ -1,7 +1,26 @@
 (ns clj-ai.core-test
   (:require [clojure.test :refer :all]
+            [clojure.pprint :refer [pprint]]
             [clj-ai.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+#_(deftest query-a-neural-network
+  (let [network [[[0.11 0.12]
+                  [0.13 0.14]]
+                 [[0.21 0.22]
+                  [0.23 0.24]]
+                 [[0.31 0.32]
+                  [0.33 0.34]]]
+        input [0.2 0.4 0.6]]
+    (pprint (query network input))))
+
+(deftest the-network-must-be-a-vector
+  (is (thrown-with-msg?
+        clojure.lang.ExceptionInfo
+        #"The network is not valid"
+        (query "network" [1 2 3]))))
+
+(deftest the-network-must-be-a-vector-of-vectors
+  (is (thrown-with-msg?
+        clojure.lang.ExceptionInfo
+        #"The network is not valid"
+        (query ["one" "two"] [1 2 3]))))
