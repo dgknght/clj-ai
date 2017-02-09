5c6662fc6141d5936080310518dc3ab37475c045
Doug Knight <doug.knight@livingsocial.com>
2017-02-09 11:00:13 -0600

    extract shared network value

--- test/clj_ai/core_test.clj
+++ test/clj_ai/core_test.clj
@@ -3,14 +3,16 @@
             [clojure.pprint :refer [pprint]]
             [clj-ai.core :refer :all]))
 
-#_(deftest query-a-neural-network
-  (let [network [[[0.11 0.12]
-                  [0.13 0.14]]
-                 [[0.21 0.22]
-                  [0.23 0.24]]
-                 [[0.31 0.32]
-                  [0.33 0.34]]]
-        input [0.2 0.4 0.6]]
+(def ^:private network
+  [[[0.11 0.12]
+    [0.13 0.14]]
+   [[0.21 0.22]
+    [0.23 0.24]]
+   [[0.31 0.32]
+    [0.33 0.34]]])
+
+(deftest query-a-neural-network
+  (let [input [5 9]]
     (pprint (query network input))))
 
 (deftest the-network-must-be-a-vector
@@ -24,3 +26,9 @@
         clojure.lang.ExceptionInfo
         #"The network is not valid"
         (query ["one" "two"] [1 2 3]))))
+
+(deftest the-input-must-be-a-vector-of-numbers
+  (is (thrown-with-msg?
+        clojure.lang.ExceptionInfo
+        #"The input is not valid"
+        (query network :not-a-vector))))
