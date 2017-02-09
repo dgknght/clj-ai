44cea4a8024d963083f1d337acb94ca04a1a1b29
Doug Knight <doug.knight@livingsocial.com>
2017-02-09 16:02:41 -0600

    start work on training

--- src/clj_ai/core.clj
+++ src/clj_ai/core.clj
@@ -31,7 +31,10 @@
 
 (defn query
   "Accepts a matrix of link weights an some
-  input and returns the neural network output"
+  input and returns the neural network output
+
+  network - a vector of matrices (vector of vectors) represending weights of links between nodes
+  input - a vector a numbers"
   [network input]
   (when-not (s/valid? ::network network)
     (throw (ex-info "The network is not valid" {:network network})))
@@ -47,7 +50,23 @@
 (defn train
   "Accepts a matrix of link weights and some
   input data and returns the updates matrix
-  of weights"
-  [network input]
-  {:network (s/valid? network)
-   :input (s/valid? input)})
+  of weights
+
+  network - a vector of matrices (vector of vectors) represending weights of links between nodes
+  input - a vector a numbers
+  target - a vector of numbers representing the desired response"
+  [network input target]
+  (when-not (s/valid? ::network network)
+    (throw (ex-info "The network is not valid" {:network network})))
+  (when-not (s/valid? ::input input)
+    (throw (ex-info "The input is not valid" {:input input})))
+  (when-not (s/valid? ::input target)
+    (throw (ex-info "The target is not valid" {:target target})))
+  (let [result (query network input)
+        error (->> (interleave target result)
+                   (partition 2)
+                   (map (partial apply -)))]
+    (->> network
+         (reduce (fn [reverse-input layer]
+                   )
+                 error))))
--- test/clj_ai/core_test.clj
+++ test/clj_ai/core_test.clj
@@ -1,31 +1,37 @@
 (ns clj-ai.core-test
   (:require [clojure.test :refer :all]
             [clojure.pprint :refer [pprint]]
+            [clojure.core.matrix :refer [shape]]
             [clj-ai.core :refer :all]))
 
 (def ^:private network
-  [[[0.11 0.12]
-    [0.13 0.14]]
-   [[0.21 0.22]
-    [0.23 0.24]]
-   [[0.31 0.32]
-    [0.33 0.34]]])
+  [[[0.11M 0.12M]
+    [0.13M 0.14M]]
+   [[0.21M 0.22M]
+    [0.23M 0.24M]]
+   [[0.31M 0.32M]
+    [0.33M 0.34M]]])
 
 (deftest query-a-neural-network
   (let [input [5 9]]
-    (pprint (query network input))))
+    (is (= [2] (shape (query network input))) "The result is a vector of numbers")))
+
+(deftest train-a-neural-network
+  (let [result (train network [5M 3M] [0.05M 0.02M])]
+    (is (vector? result) "A vector is returned")
+    (is (= (shape network) (shape result)) "The result is the same shape as the input network")))
 
 (deftest the-network-must-be-a-vector
   (is (thrown-with-msg?
         clojure.lang.ExceptionInfo
         #"The network is not valid"
-        (query "network" [1 2 3]))))
+        (query "network" [1M 2M 3M]))))
 
 (deftest the-network-must-be-a-vector-of-vectors
   (is (thrown-with-msg?
         clojure.lang.ExceptionInfo
         #"The network is not valid"
-        (query ["one" "two"] [1 2 3]))))
+        (query ["one" "two"] [1M 2M 3M]))))
 
 (deftest the-input-must-be-a-vector-of-numbers
   (is (thrown-with-msg?
